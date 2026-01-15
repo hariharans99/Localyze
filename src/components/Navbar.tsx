cbbfa127b977b066b5bc7b5580c0042f853cfca4
hari-io <harisugavan@gmail.com>
2026-01-15 21:40:06 +0530

    feat: Updated brand logo with filled center and clean icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export const Navbar = () => {
                 position: 'relative'
             }}>
                 <Link to="/" style={{ display: 'flex', alignItems: 'center', gap: '0.75rem', fontSize: '1.25rem', fontWeight: 700 }}>
-                    <img src="/logo.png" alt="Localyze" style={{ height: '36px', width: 'auto' }} />
+                    <img src="/logo_brand_v2.png" alt="Localyze" style={{ height: '40px', width: 'auto' }} />
                     <span style={{ background: 'linear-gradient(to right, #6366f1, #a855f7)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>Localyze</span>
                 </Link>
 
